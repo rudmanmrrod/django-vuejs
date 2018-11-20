16b08d10b33d0c65a1f39bd57832a98dee2ab7b7
Rudmanmrrod <rudmanmrrod@gmail.com>
2018-11-20 09:50:13 -0400

    Agregadas las validaciones de la parte 3 del tutorial

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', IndexTemplate.as_view()),
-    path('create', CreateUserView.as_view()),
+    path('', IndexTemplate.as_view(), name='index'),
+    path('create', CreateUserView.as_view(), name='user_profile_create'),
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -12,22 +12,21 @@ class CreateUserView(FormView):
   form_class = RegisterForm
 
   def form_valid(self,form):
-    user = User()
-    user.username = form.cleaned_data['username']
-    user.email = form.cleaned_data['email']
-    user.first_name = form.cleaned_data['first_name']
-    user.last_name = form.cleaned_data['last_name']
-    user.set_password(form.cleaned_data['password1'])
-    user.save()
-    user_profile = UserProfile()
-    user_profile.dni_number =  form.cleaned_data['dni_number']
-    user_profile.address =  form.cleaned_data['address']
-    user_profile.zip_code =  form.cleaned_data['zip_code']
-    user_profile.gender =  form.cleaned_data['gender']
-    user_profile.user = user
-    user_profile.save()
-    return JsonResponse({'message':'Create Successfully'})
+    #user = User()
+    #user.username = form.cleaned_data['username']
+    #user.email = form.cleaned_data['email']
+    #user.first_name = form.cleaned_data['first_name']
+    #user.last_name = form.cleaned_data['last_name']
+    #user.set_password(form.cleaned_data['password1'])
+    #user.save()
+    #user_profile = UserProfile()
+    #user_profile.dni_number =  form.cleaned_data['dni_number']
+    #user_profile.address =  form.cleaned_data['address']
+    #user_profile.zip_code =  form.cleaned_data['zip_code']
+    #user_profile.gender =  form.cleaned_data['gender']
+    #user_profile.user = user
+    #user_profile.save()
+    return JsonResponse({'message':'Create Successfully','valid':True})
 
   def form_invalid(self,form):
-    return JsonResponse({'error':form.errors})
-    return super().form_invalid(form)
+    return JsonResponse({'error':form.errors,'valid':False})
